413c4163cb43e639abdf065fb97d50b992753e9b
Marcelo <mrangel@itexico.com>
2020-04-08 20:52:19 -0500

    Add project files.

---
+++ BeautyProds/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BeautyProds.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BeautyProds/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BeautyProds.WebApiApplication" Language="C#" %>
