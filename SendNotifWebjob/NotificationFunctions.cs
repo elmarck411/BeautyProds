3b70b7025e5ed42b2c20712bde370c96aee964a2
Marcelo <mrangel@itexico.com>
2020-06-21 02:49:34 -0500

    Create and configure Webjob, before publishing

---
+++ SendNotifWebjob/NotificationFunctions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendNotifWebjob
+{
+    public class NotificationFunctions
+    {
+        [NoAutomaticTrigger]
+        public void SendNotifications(TextWriter log)
+        {
+            log.WriteLine("SendNotifications Logic");
+        }
+    }
+}
